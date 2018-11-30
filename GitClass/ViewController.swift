df0b60e0095e1ec74fcca06a80821a81b24f8a5b
Thomas E Cowern <thomas.e.cowern@gmail.com>
2018-11-30 15:13:29 -0700

    set up storyboard

--- GitClass/ViewController.swift
+++ GitClass/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timerLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func timerButton(_ sender: Any) {
+        
+    }
+    
 }
 
