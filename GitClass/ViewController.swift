c1e94927f6b7fcde40a7ad71c6a507b44760753a
Thomas E Cowern <thomas.e.cowern@gmail.com>
2018-11-30 13:55:37 -0700

    Initial Commit

---
+++ GitClass/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitClass
+//
+//  Created by Thomas Cowern New on 11/30/18.
+//  Copyright © 2018 vetDevHouse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
